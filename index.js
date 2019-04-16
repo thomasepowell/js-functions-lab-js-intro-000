3f4f5a086efbdad2e6aaceb2092c645ab2524fc1
glorious-tier-7619 <thomasepowell@icloud.com>
2019-04-16 14:13:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function holidayCountdown(holiday, days){
   return `It\'s ${days} days until ${holiday}!`
 }
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+ function happyHolidays(){
+   return "Happy holidays!"
+ }
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
