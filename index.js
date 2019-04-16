8319efb33ed2d69780648c5baa9e695c8166443d
glorious-tier-7619 <thomasepowell@icloud.com>
2019-04-16 13:43:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}// write your code below!
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"
+}// write your code below!
+
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
+}
